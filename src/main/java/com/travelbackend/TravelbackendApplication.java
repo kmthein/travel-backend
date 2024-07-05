61c44d81e55d63c1ae1ebd30cb5f8c5ae83a7980
kmthein <kmthein96.mm@gmail.com>
2024-07-05 15:58:04 +0630

    project setup

---
+++ src/main/java/com/travelbackend/TravelbackendApplication.java
@@ -0,0 +1,13 @@
+package com.travelbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TravelbackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TravelbackendApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/travelbackend/TravelbackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.travelbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TravelbackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
