58828fae0d0be9217caac28792f9b9a74a7eb54e
Nan Aye Thandar Lin <nanayethandarlin869@gmail.com>
2024-07-10 13:02:02 +0630

    entity and dao done

--- src/main/java/com/travelbackend/entity/BaseEntity.java
+++ src/main/java/com/travelbackend/entity/BaseEntity.java
@@ -1,10 +1,12 @@
 package com.travelbackend.entity;
 
 import jakarta.persistence.Column;
+import jakarta.persistence.MappedSuperclass;
 
 import java.time.LocalDateTime;
 import java.time.LocalTime;
 
+@MappedSuperclass
 public class BaseEntity {
     @Column(name = "is_delete")
     private boolean isDelete;
