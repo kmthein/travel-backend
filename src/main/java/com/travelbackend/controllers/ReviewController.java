e6718884cdf67605c101fd25b2d581ad2dc9bff6
Nan Aye Thandar Lin <nanayethandarlin869@gmail.com>
2024-07-12 15:44:32 +0630

    Accommodation and Review done

---
+++ src/main/java/com/travelbackend/controllers/ReviewController.java
@@ -0,0 +1,50 @@
+package com.travelbackend.controllers;
+
+import com.travelbackend.entity.Review;
+import com.travelbackend.services.ReviewService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@CrossOrigin
+@RestController
+@RequestMapping("api")
+public class ReviewController {
+
+    private ReviewService reviewService;
+
+    @Autowired
+    public ReviewController(ReviewService reviewService){
+        this.reviewService = reviewService;
+    }
+
+    @PostMapping("/review")
+    public ResponseEntity<?> createReview(@ModelAttribute Review review,@RequestParam int userId,@RequestParam int destinationId){
+        reviewService.saveReview(review,userId,destinationId);
+        return ResponseEntity.ok("Inserted Review!");
+    }
+
+    @GetMapping("/review")
+    public List<Review> findAllReview(){
+        return reviewService.findAllReview();
+    }
+
+    @GetMapping("/review/{id}")
+    public Review findReviewById(@PathVariable int id){
+        return reviewService.findReviewById(id);
+    }
+
+    @PutMapping("/review/{id}")
+    public ResponseEntity<?> updateReview(@ModelAttribute Review review,@PathVariable int id,@RequestParam int userId,@RequestParam int destinationId){
+        reviewService.updateReview(review,id,userId,destinationId);
+        return ResponseEntity.ok("Review Updated! ");
+    }
+
+    @PostMapping("/review/{id}")
+    public ResponseEntity<?> deleteReview(@PathVariable int id){
+        reviewService.deleteReview(id);
+        return ResponseEntity.ok("Review Deleted!");
+    }
+}
---
+++ src/main/java/com/travelbackend/services/AccommodationServiceImpl.java
@@ -0,0 +1,72 @@
+package com.travelbackend.services;
+
+import com.travelbackend.dao.AccommodationDAO;
+import com.travelbackend.dao.RoomDAO;
+import com.travelbackend.entity.Accommodation;
+import com.travelbackend.entity.Room;
+import com.travelbackend.exception.ResourceNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class AccommodationServiceImpl implements AccommodationService{
+
+    private AccommodationDAO accommodationDAO;
+
+    private RoomDAO roomDAO;
+
+    @Autowired
+    public AccommodationServiceImpl(AccommodationDAO accommodationDAO,RoomDAO roomDAO){
+        this.accommodationDAO = accommodationDAO;
+        this.roomDAO = roomDAO;
+    }
+
+    @Override
+    public void saveAccommodation(Accommodation accommodation, int roomId) {
+        Room room = roomDAO.findRoomById(roomId);
+        if(room == null){
+            throw new ResourceNotFoundException("There are no available rooms in a hotel!");
+        }
+        accommodation.setRoom(room);
+        accommodationDAO.save(accommodation);
+    }
+
+    @Override
+    public List<Accommodation> findAllAccommodation() {
+        return accommodationDAO.findAll();
+    }
+
+    @Override
+    public Accommodation findAccommodationById(int accommodationId) {
+        return accommodationDAO.findAccommodationById(accommodationId);
+    }
+
+    @Override
+    public void updateAccommodation(Accommodation accommodation, int accommodationId, int roomId) {
+        Accommodation accommo = accommodationDAO.findAccommodationById(accommodationId);
+        if(accommo == null){
+            throw new ResourceNotFoundException("No data in Accommodation Table");
+        }
+        accommo.setCheckIn(accommodation.getCheckIn());
+        accommo.setCheckOut(accommodation.getCheckOut());
+        accommo.setTotalPerson(accommodation.getTotalPerson());
+        accommo.setTotalPrice(accommodation.getTotalPrice());
+        Room room = roomDAO.findRoomById(roomId);
+        if(room == null){
+            throw new ResourceNotFoundException("No free Room! Please kindly take another!");
+        }
+        accommo.setRoom(room);
+        accommodationDAO.update(accommo);
+    }
+
+    @Override
+    public void deleteAccommodation(int accommodationId) {
+        Accommodation accommodation = accommodationDAO.findAccommodationById(accommodationId);
+        if(accommodation != null){
+           accommodation.setDelete(true);
+        }
+        accommodationDAO.update(accommodation);
+    }
+}
